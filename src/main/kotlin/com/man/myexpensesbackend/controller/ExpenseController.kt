efac38ab28d4b2c630235234032ed41c65f1d962
Dev-Micah <atembamicah@gmail.com>
2026-04-19 22:54:56 +0300

    Controller and database configuration

---
+++ src/main/kotlin/com/man/myexpensesbackend/controller/ExpenseController.kt
@@ -0,0 +1,37 @@
+package com.man.myexpensesbackend.controller
+
+import com.man.myexpensesbackend.dtos.ExpenseRequestDTO
+import com.man.myexpensesbackend.service.ExpenseService
+import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/api/expenses")
+class ExpenseController(private val service: ExpenseService) {
+
+    @GetMapping
+    fun getAllExpenses() = service.getAllExpenses()
+
+    @GetMapping("/{id}")
+    fun getById(@PathVariable id: Long) = service.findById(id)
+
+    @PostMapping
+    fun create(@RequestBody dto: ExpenseRequestDTO) =
+        service.createExpense(dto)
+
+    @PutMapping("/{id}")
+    fun update(
+        @PathVariable id: Long,
+        @RequestBody dto: ExpenseRequestDTO
+    ) = service.updateExpense(id, dto)
+
+    @DeleteMapping("/{id}")
+    fun delete(@PathVariable id: Long) =
+        service.deleteExpense(id)
+}
